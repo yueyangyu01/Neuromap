4717ceb3c55e0af281fd0ba775c49f5b5b5b3ed0
Yue Yang <kevlee998@gmail.com>
2024-01-16 10:02:18 -0500

    initial commit

---
+++ mydb.py
@@ -0,0 +1,14 @@
+import mysql.connector
+
+dataBase = mysql.connector.connect(
+	host = "localhost",
+	user = "root",
+	passwd = "password123"
+
+)
+
+cursorObject = dataBase.cursor()
+
+cursorObject.execute("CREATE DATABASE storage")
+
+print("all done")
